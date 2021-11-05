59e484d78d1388c31847f1e90504a80c3ab322de
Mark Page <mark.page@burdenandburden.co.uk>
2021-11-05 00:28:42 +0000

    Corrects comments

--- url-hyperlink.php
+++ url-hyperlink.php
@@ -1,20 +1,13 @@
 <?php
 
 class UrlHyperlink {
-	/** Get options to display edit field
-	* @param string table name
-	* @param array single field from fields()
-	* @param string attributes to use inside the tag
-	* @param string
-	* @return string custom input field or empty string for default
-	*/
     /*
-        When using Adminer select tool, cell values that are in good
-        URL format get converted into <a>link</a>s.
+        When using Adminer main select view, a cell value that
+        looks like a URL gets converted into a <a>link</a>.
         Sadly in other views this is not the case:
           * Data returned from a stored procedure
           * Data returned from arbitrary SQL
-        This plug-in rectifies that oversight.
+        This plug-in should rectify that oversight.
     */
 }
 
